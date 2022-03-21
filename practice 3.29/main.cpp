7b8ad7354ccd8336795894b6523f64cb57b36763
junklasagna <redlinetango@gmail.com>
2022-03-21 19:50:58 +0800

    file uploaded

---
+++ practice 3.29/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+// this practice was for testing what will happen if one put "true" for while statement
+int main()
+{   int x;
+    x = 1;
+    while ( true )
+           {
+              x++;
+               cout << x* 2 << endl;
+
+           }
+    return 0;
+}
